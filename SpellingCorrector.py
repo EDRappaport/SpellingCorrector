ba425ba118b87166fe60af97b23a40b8ca29f391
EDRappaport <ElliRappaport@gmail.com>
2014-04-18 01:47:54 -0400

    single dist edits

--- SpellingCorrector.py
+++ SpellingCorrector.py
@@ -9,6 +9,26 @@ def edits1(word):
   splits = []; deletes = []; transposes = [];
   replaces = []; inserts = [];
 
+  for i in range(0, len(word)+1):
+    splits.append([(word[:i], word[i:])])
+
+  for a, b in splits:
+    if b:
+      deletes.append(a + b[1:])
+
+  for a, b in splits:
+    if (len(b)>1):
+      transposes.append(a + b[1] + b[0] + b[2:])
+
+  for a, b in splits:
+    for c in string.ascii_lowercase:
+      if b:
+        replaces.append(a + c + b[1:])
+
+  for a, b in splits:
+      for c in string.ascii_lowercase:
+        inserts.append(a + c + b)
+
    return set(deletes + transposes + replaces + inserts)
 
 
