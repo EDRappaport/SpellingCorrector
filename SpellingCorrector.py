c5438c8de78ffd335d98d5653c7b5b2214d26acc
EDRappaport <ElliRappaport@gmail.com>
2014-04-20 15:49:01 -0400

    conditional probs and small bug fix

--- SpellingCorrector.py
+++ SpellingCorrector.py
@@ -1,35 +1,41 @@
 import re, collections
 import sys
 import string
+import ReadCondProbs
 
 #alphabet = 'abcdefghijklmnopqrstuvwxyz'
 symbols = '\'- '
+condProbs = ReadCondProbs.getCondProbs('count_1edit.txt', 'count_2l.txt')
 
 def edits1(word):
   splits = []; deletes = []; transposes = [];
   replaces = []; inserts = [];
 
   for i in range(0, len(word)+1):
-    splits.append([(word[:i], word[i:])])
+    splits.append((word[:i], word[i:]))
 
+  # Deletions
   for a, b in splits:
     if b:
       deletes.append(a + b[1:])
 
+  # Transpositions
   for a, b in splits:
     if (len(b)>1):
       transposes.append(a + b[1] + b[0] + b[2:])
 
+  # Substitutions
   for a, b in splits:
     for c in string.ascii_lowercase:
       if b:
         replaces.append(a + c + b[1:])
 
+  # Insertions
   for a, b in splits:
       for c in string.ascii_lowercase:
         inserts.append(a + c + b)
 
-   return set(deletes + transposes + replaces + inserts)
+  return set(deletes + transposes + replaces + inserts)
 
 
 #MAIN:
@@ -42,4 +48,4 @@ word = str(sys.argv[1])
 
 candidates = edits1(word)
 
-print candidates
+#print candidates
